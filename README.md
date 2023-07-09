372c86634a2586d98837b9a38fce70c3e3a00535
Daniel Schmid <daniel.schmid1980@icloud.com>
2023-07-09 21:29:59 +0200

    feat: add emailclient and maintainable test suite

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ http://127.0.0.1:8000/subscriptions
 Test health-check
 
 curl -v http://127.0.0.1:8000/health_check
+
+Test send_email
+
+curl "https://api.postmarkapp.com/email" \
+-X POST \
+-H "Accept: application/json" \
+-H "Content-Type: application/json" \
+-H "X-Postmark-Server-Token: server token" \
+-d '{
+"From": "sender@example.com",
+"To": "receiver@example.com",
+"Subject": "Postmark test",
+"TextBody": "Hello dear Postmark user.",
+"HtmlBody": "<html><body><strong>Hello</strong> dear Postmark user.</body></html>"
+}'
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,12 +1,9 @@
-use actix_web::{
-    web::{self},
-    HttpResponse,
-};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
+use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use std::convert::{TryFrom, TryInto};
 use uuid::Uuid;
-
-use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -24,6 +21,7 @@ impl TryFrom<FormData> for NewSubscriber {
     }
 }
 
+#[allow(clippy::async_yields_async)]
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(form, pool),
@@ -31,14 +29,12 @@ impl TryFrom<FormData> for NewSubscriber {
         subscriber_email = %form.email,
         subscriber_name = %form.name
     )
-)] // use tracing::Instrument;
-
+)]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let new_subscriber = match form.0.try_into() {
         Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
@@ -49,13 +45,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     name = "Saving new subscriber details in the database",
     skip(new_subscriber, pool)
 )]
-
 pub async fn insert_subscriber(
     pool: &PgPool,
     new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
-        r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
+        r#"
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+            "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
